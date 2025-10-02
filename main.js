53531222b73572e97d55a890eeb69f6f687f71de
dima <dima.kuzov@gmail.com>
2025-10-02 13:11:40 +0200

    use old logig

--- main.js
+++ main.js
@@ -743,13 +743,12 @@ async function main() {
         viewMatrix = JSON.parse(decodeURIComponent(location.hash.slice(1)));
         carousel = false;
     } catch (err) {}
-//    const url = new URL(
-//        // "nike.splat",
-//        // location.href,
-//        params.get("url") || "train.splat",
-//        "https://huggingface.co/cakewalk/splat-data/resolve/main/",
-//    );
-    const url = "./assets/my.splat";
+    const url = new URL(
+        // "nike.splat",
+        // location.href,
+        params.get("url") || "my.splat",
+        "https://dimakuzov.github.io/splat-viewer/assets/",
+    );
     const req = await fetch(url, {
         mode: "cors", // no-cors, *cors, same-origin
         credentials: "omit", // include, *same-origin, omit
